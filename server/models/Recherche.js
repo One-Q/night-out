bf565a090f4a9d73f2c2c690127666cdf85a07f1
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-10-19 21:14:04 +0200

    Ajout Schema event

--- server/models/Recherche.js
+++ server/models/Recherche.js
@@ -2,7 +2,20 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const RechercheSchema = new Schema({
-
+  name: { type: 'String', required: true },
+  picture: { type: 'String' },
+  description: { type: 'String', required: true },
+  category: { type: 'String', required: true },
+  location: {
+    city: { type: 'String', required: true},
+    street: { type: 'String', required: true},
+    latitude: { type: 'Number', required: true },
+    longitude: { type: 'Number', required: true },
+  },
+  slug: { type: 'String', required: true },
+  cuid: { type: 'String', required: true },
+  startTime: { type: 'Date', required: true },
+  dateAdded: { type: 'Date', default: Date.now, required: true },
 });
 
 export default mongoose.model('Recherche', RechercheSchema);
