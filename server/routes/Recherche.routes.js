6991dea3451808b0c1cafd98d8b6e72f1294b8ae
Romain Descamps <romain.descamps@student.vinci.be>
2017-10-20 00:35:24 +0200

    Ajout de la route et du controller

--- server/routes/Recherche.routes.js
+++ server/routes/Recherche.routes.js
@@ -3,5 +3,10 @@ import * as RechercheController from '../controllers/Recherche.controller';
 
 const router = new Router();
 
+// Get all events
+router.route('/events').get(RechercheController.getEvents)
+
+// Get on event by name
+router.route('/events:name').get(RechercheController.getEvent)
 
 export default router;
