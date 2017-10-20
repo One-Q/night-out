d593b321a5a06dcff6c3b077016d3bd3709a2df3
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-10-20 09:26:09 +0200

    change name to slug getEvent event.controller

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -6,7 +6,7 @@ import Event from '../models/event';
  * @param res
  */
 export function getEvent(req, res) {
-  Event.findOne({ name: req.params.name }).exec((err, event) => {
+  Event.findOne({ slug: req.params.name }).exec((err, event) => {
     if (err) {
       res.status(500).send(err);
     }
