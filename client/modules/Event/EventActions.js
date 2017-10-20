d76738077440389aac9c633e06a874fef80d360d
Romain Descamps <romain.descamps@student.vinci.be>
2017-10-20 09:39:24 +0200

    Complétion eventAction client

--- client/modules/Event/EventActions.js
+++ client/modules/Event/EventActions.js
@@ -1,3 +1,35 @@
+import callApi from '../../util/apiCaller';
+
 // Export Constants
+export const ADD_EVENT = 'ADD_EVENTS';
+export const ADD_EVENTS = 'ADD_EVENTS';
+export const DELETE_EVENT = 'DELETE_EVENT';
 
 // Export Actions
+export function addEvent(event) {
+    return {
+        type: ADD_EVENT,
+        event,
+    };
+}
+
+export function fetchEvents() {
+    return (dispatch) => {
+        return callApi('events').then(res => {
+            dispatch(addEvents(res.events));
+        });
+    };
+}
+
+export function addEvents(events) {
+    return {
+        type: ADD_EVENTS,
+        events,
+    };
+}
+
+export function fetchEvent(slug) {
+    return (dispatch) => {
+        return callApi(`events/${slug}`).then(res => dispatch(addEvent(res.event)));
+    };
+}
