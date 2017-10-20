234603cdb7381e1376ef22f7259cc258f403b3d7
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-10-20 14:16:14 +0200

    Reindent

--- client/modules/Event/EventActions.js
+++ client/modules/Event/EventActions.js
@@ -7,29 +7,29 @@ export const DELETE_EVENT = 'DELETE_EVENT';
 
 // Export Actions
 export function addEvent(event) {
-    return {
-        type: ADD_EVENT,
-        event,
-    };
+	return {
+		type: ADD_EVENT,
+		event,
+	};
 }
 
-export function fetchEvents() {
-    return (dispatch) => {
-        return callApi('events').then(res => {
-            dispatch(addEvents(res.events));
-        });
-    };
+export function fetchEvents() { 
+	return (dispatch) => {
+		return callApi('events').then(res => {
+			dispatch(addEvents(res.events));
+		});
+	};
 }
 
 export function addEvents(events) {
-    return {
-        type: ADD_EVENTS,
-        events,
-    };
+	return {
+		type: ADD_EVENTS,
+		events,
+	};
 }
 
 export function fetchEvent(slug) {
-    return (dispatch) => {
-        return callApi(`events/${slug}`).then(res => dispatch(addEvent(res.event)));
-    };
+	return (dispatch) => {
+		return callApi(`events/${slug}`).then(res => dispatch(addEvent(res.event)));
+	};
 }
