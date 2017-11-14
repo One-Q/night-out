5819f5d65600ddad617471e69d51503a81fc5c80
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-11-14 16:38:01 +0100

    Authentification module

---
+++ client/modules/Authentification/LogIn/Login.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { Card, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+
+const Login = ({
+  onSubmit,
+  onChange,
+  errors,
+  user
+}) => (
+  <div>
+    <h2>Login</h2>
+  </div>
+);
+
+Login.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired
+};
+
+export default Login;
---
+++ client/modules/Authentification/SignUp/SignUp.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { Card, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+
+const SignUp = ({
+  onSubmit,
+  onChange,
+  errors,
+  user,
+}) => (
+  <div>
+    <h2>Sign up</h2>
+  </div>
+);
+
+SignUp.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+};
+
+export default SignUp;
