3a021ff41173380e3c009eb54cc553dd2a6c0e14
Romain Descamps <romain.descamps@student.vinci.be>
2017-11-27 18:10:01 +0100

    Sécurisation des requêtes entrantes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,25 +2,20 @@ import User from '../models/user';
 import cuid from 'cuid';
 import slug from 'limax';
 import nodemailer from 'nodemailer';
-
-const regUsername = /^[^\d\s][\S0-9]{5,14}$/;
-const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const regPassword = /^\S{8,64}$/;
+import regex from '../util/regex';
+import validator from 'express-validator';
 
 export function signUp(req, res) {
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
-    if (!username || !email || !password)
-        return res.status(400).json({ error: 'Veuillez remplir tous les champs.' });
-    //Username 6-15 carac ne peut pas commencer par un chiffre
-    if (!regUsername.test(username))
-        return res.status(400).json({ error: 'Veuillez entrer un nom d\'utilisateur valide.' });
-    if (!regEmail.test(email))
-        return res.status(400).json({ error: 'Veuillez entrer une adresse email valide.' });
-    //mdp 8-64 carac sans espaces
-    if (!regPassword.test(password))
-        return res.status(400).json({ error: 'Veuillez entrer un mot de passe valide.' });
+    req.checkBody('username', 'Veuillez entrer un nom d\'utilisateur valide.').matches(regex.regUsername);     // //Username 6-15 carac ne peut pas commencer par un chiffre
+    req.checkBody('email', 'Veuillez entrer une adresse email valide.').isEmail();
+    req.checkBody('password', 'Veuillez entrer un mot de passe valide.').matches(regex.regPassword);     // //mdp 8-64 carac sans espaces
+    let errors = req.validationErrors();
+    if (errors) {
+        return res.status(400).json({ error: errors[0]['msg'] });
+    }
     Promise.all([checkEmail(email), checkUsername(username)]).then(() => {
         const newUser = new User({ username: username, email: email, slug: slug(username.toLowerCase(), { lowercase: true }), cuid: cuid() });
         newUser.password = newUser.generateHash(password);
@@ -40,12 +35,12 @@ export function signUp(req, res) {
                     html: `<p>You are signed up : ${newUser.username}</p>`// plain text body
                 };
                 transporter.sendMail(mailOptions, function (err, info) {
-                    if(err)
-                      console.log(err)
+                    if (err)
+                        console.log(err)
                     else
-                      console.log(info);
-                 });
-                 return res.status(200).json({success : "ok"});
+                        console.log(info);
+                });
+                return res.status(200).json({ success: "ok" });
             }
             console.log('Erreur d\'insertion user : ' + error);
             return res.status(500).json({ error: 'Erreur interne' });
@@ -59,10 +54,12 @@ export function changePassword(req, res) {
     let cuid = req.user.id;
     let oldPassword = req.body.oldPassword;
     let newPassword = req.body.newPassword;
-    if (!cuid || !oldPassword || !newPassword)
-        return res.status(400).json({ error: 'Veuillez remplir tous les champs.' });
-    if (!regPassword.test(newPassword))
-        return res.status(400).json({ error: 'Veuillez entrer un nouveau mot de passe valide.' });
+    req.checkBody('oldPassword', 'Veuillez entrer votre ancien mot de passe.').matches(regex.regPassword);     // //mdp 8-64 carac sans espaces
+    req.checkBody('newPassword', 'Veuillez entrer un nouveau mot de passe valide.').matches(regex.regPassword);     // //mdp 8-64 carac sans espaces
+    let errors = req.validationErrors();
+    if (errors) {
+        return res.status(400).json({ error: errors[0]['msg'] });
+    }
     User.findOne({ cuid }).then((user) => {
         if (!user) // Pas sensé arriver
             return res.status(500).json({ error: 'Erreur interne.' });
@@ -72,7 +69,7 @@ export function changePassword(req, res) {
         user.save((err) => {
             if (err)
                 return res.status(500).json({ error: 'Erreur interne' });
-            return res.status(200).json({success : "ok"});
+            return res.status(200).json({ success: "ok" });
         })
     });
 }
