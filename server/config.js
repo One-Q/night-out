bb61e8b68c72e0e7098ef0c6242227de5071f867
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2017-12-08 09:02:44 +0100

    db

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://heroku_q73bd7nd:ifkof4c5qtc4ls2rblt1ac8mu8@ds117965.mlab.com:17965/heroku_q73bd7nd',/*'mongodb://localhost:27017/mern-starter',*/
+  mongoURL: process.env.MONGO_URL || /*'mongodb://heroku_q73bd7nd:ifkof4c5qtc4ls2rblt1ac8mu8@ds117965.mlab.com:17965/heroku_q73bd7nd',*/'mongodb://localhost:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
